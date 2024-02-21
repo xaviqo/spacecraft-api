6bcd2475a381c71744eb54be5c94fd736338bb13
Xavi Quinones <mail@xavi.tech>
2024-02-21 21:42:55 +0100

    Created basic API structure

---
+++ SpacecraftAPI/src/main/java/tech/xavi/spacecraft/SpacecraftApiApplication.java
@@ -0,0 +1,13 @@
+package tech.xavi.spacecraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpacecraftApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpacecraftApiApplication.class, args);
+    }
+
+}
