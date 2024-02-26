52491ec3dbc672897d56e224abe2f015bb24df41
Xavi Quinones <mail@xavi.tech>
2024-02-26 01:07:41 +0100

    Added OpenAPI definition

---
+++ SpacecraftAPI/src/main/java/tech/xavi/spacecraft/configuration/OpenAPIConfiguration.java
@@ -0,0 +1,43 @@
+package tech.xavi.spacecraft.configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+        info = @Info(
+                contact = @Contact(
+                        name = "Xavi",
+                        url = "https://xavi.tech"
+                ),
+                description = "Spacecrafts REST API Sandbox",
+                title = "OpenApi specification for Spacecraft REST API",
+                version = "1.0.0"
+        ),
+        servers = {
+                @Server(
+                        description = "Local ENV",
+                        url = "http://localhost:8080"
+                ),
+        },
+        security = {
+                @SecurityRequirement(
+                        name = "bearerAuth"
+                )
+        }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        description = "JWT auth description",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+public class OpenAPIConfiguration {
+}
